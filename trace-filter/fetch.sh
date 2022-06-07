c7dbef791ccdd4cdbff1663e99fd56a6464eed5f
Eva Pace <eba.pachi@gmail.com>
2022-06-07 13:42:40 -0300

    lint: Fix shellcheck SC2086 (Double quote to prevent globbing and word splitting)

--- trace-filter/fetch.sh
+++ trace-filter/fetch.sh
@@ -18,4 +18,4 @@ FROM_TO_BLOCK="0xC814F8"
 # - 0xc5e9ddebb09cd64dfacab4011a0d5cedaf7c9bdb = ProofOfHumanity
 # - 0x988b3A538b618C7A603e1c11Ab82Cd16dbE28069 = KlerosLiquid
 
-curl --data '{"method":"trace_filter","params":[{"fromBlock":"'"$FROM_TO_BLOCK"'","toBlock":"'"$FROM_TO_BLOCK"'","toAddress":["0xc5e9ddebb09cd64dfacab4011a0d5cedaf7c9bdb", "0x988b3a538b618c7a603e1c11ab82cd16dbe28069"]}],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST $MAINNET_URL > "trace-filter/toAddress$FROM_TO_BLOCK.json"
+curl --data '{"method":"trace_filter","params":[{"fromBlock":"'"$FROM_TO_BLOCK"'","toBlock":"'"$FROM_TO_BLOCK"'","toAddress":["0xc5e9ddebb09cd64dfacab4011a0d5cedaf7c9bdb", "0x988b3a538b618c7a603e1c11ab82cd16dbe28069"]}],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST "$MAINNET_URL" > "trace-filter/toAddress$FROM_TO_BLOCK.json"
